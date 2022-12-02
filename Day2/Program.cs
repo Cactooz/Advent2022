b84dc63de5a490185f269b4339e10efd52139ad6
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-12-02 10:07:48 +0100

    Add Day2 Part2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,6 +6,7 @@
             string[] data = File.ReadAllLines(file);
 
             Part1(data);
+            Part2(data);
         }
 
         /// <summary>
@@ -45,5 +46,48 @@
 
             Console.WriteLine(total);
         }
-	}
+
+        /// <summary>
+        /// Gets the points for rock paper scissors with XYZ being how the round end.
+        /// </summary>
+        /// <param name="input">Array with all played rounds.</param>
+        static void Part2(string[] input) {
+            int total = 0;
+            foreach(string round in input) {
+                string[] moves = round.Split(" ");
+
+                //If it ends in a loss
+                if(moves[1] == "X") {
+                    if(moves[0] == "A")
+                        total += 3;
+                    else if(moves[0] == "B")
+                        total += 1;
+                    else if(moves[0] == "C")
+                        total += 2;
+                }
+                //If it ends in a tie
+                else if(moves[1] == "Y") {
+                    total += 3;
+                    if(moves[0] == "A")
+                        total += 1;
+                    else if(moves[0] == "B")
+                        total += 2;
+                    else if(moves[0] == "C")
+                        total += 3;
+                }
+                //If it ends in a win
+                else if(moves[1] == "Z") {
+                    total += 6;
+                    if(moves[0] == "A")
+                        total += 2;
+                    else if(moves[0] == "B")
+                        total += 3;
+                    else if(moves[0] == "C")
+                        total += 1;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
 }
