71e8980a78037b3d9e43373782dac68527a27707
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-12-09 21:25:11 +0100

    Add Day8 Part2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -17,69 +17,127 @@
 				}
 			}
 
-            Part1(trees);
+			Part1(trees);
+			Part2(trees);
 		}
 
-        /// <summary>
-        /// Finds all trees that are visible viewing the trees from any of its sides.
-        /// </summary>
-        /// <param name="trees">All the <see cref="Tree"/> objects in two dimensional <see cref="Array"/>.</param>
+		/// <summary>
+		/// Finds all trees that are visible viewing the trees from any of its sides.
+		/// </summary>
+		/// <param name="trees">All the <see cref="Tree"/> objects in two dimensional <see cref="Array"/>.</param>
 		static void Part1(Tree[,] trees) {
 			for(int i = 1; i < trees.GetLength(0) - 1; i++) {
 				for(int j = 1; j < trees.GetLength(1) - 1 ; j++) {
-                    //Check left
-                    for(int k = 0; k < i; k++) {
-                        //Break early if a tree is taller than the current tree
-                        if(trees[k, j].Height >= trees[i, j].Height)
-                            break;
+					//Check left
+					for(int k = 0; k < i; k++) {
+						//Break early if a tree is taller than the current tree
+						if(trees[k, j].Height >= trees[i, j].Height)
+							break;
 
-                        if(k == i - 1 && trees[k, j].Height < trees[i, j].Height)
-                            trees[i, j].Visible = true;
-                    }
+						if(k == i - 1 && trees[k, j].Height < trees[i, j].Height)
+							trees[i, j].Visible = true;
+					}
 
-                    //Check up
-                    for(int k = 0; k < j; k++) {
-                        //Break early if a tree is taller than the current tree
-                        if(trees[i, k].Height >= trees[i, j].Height)
-                            break;
+					//Check up
+					for(int k = 0; k < j; k++) {
+						//Break early if a tree is taller than the current tree
+						if(trees[i, k].Height >= trees[i, j].Height)
+							break;
 
-                        if(k == j - 1 && trees[i, k].Height < trees[i, j].Height)
-                            trees[i, j].Visible = true;
-                    }
+						if(k == j - 1 && trees[i, k].Height < trees[i, j].Height)
+							trees[i, j].Visible = true;
+					}
 
-                    //Check right
-                    for(int k = trees.GetLength(0) - 1; k > j; k--) {
-                        //Break early if a tree is taller than the current tree
-                        if(trees[i, k].Height >= trees[i, j].Height)
-                            break;
+					//Check right
+					for(int k = trees.GetLength(0) - 1; k > j; k--) {
+						//Break early if a tree is taller than the current tree
+						if(trees[i, k].Height >= trees[i, j].Height)
+							break;
 
-                        if(k == j + 1 && trees[i, k].Height < trees[i, j].Height)
-                            trees[i, j].Visible = true;
-                    }
+						if(k == j + 1 && trees[i, k].Height < trees[i, j].Height)
+							trees[i, j].Visible = true;
+					}
 
-                    //Check down
-                    for(int k = trees.GetLength(1) - 1; k > i; k--) {
-                        //Break early if a tree is taller than the current tree
-                        if(trees[k, j].Height >= trees[i, j].Height)
-                            break;
+					//Check down
+					for(int k = trees.GetLength(1) - 1; k > i; k--) {
+						//Break early if a tree is taller than the current tree
+						if(trees[k, j].Height >= trees[i, j].Height)
+							break;
 
-                        if(k == i + 1 && trees[k, j].Height < trees[i, j].Height)
-                            trees[i, j].Visible = true;
-                    }
-                }
-            }
+						if(k == i + 1 && trees[k, j].Height < trees[i, j].Height)
+							trees[i, j].Visible = true;
+					}
+				}
+			}
 
-            int visible = 0;
-            //Count the amount of visible trees
-            for(int i = 0; i < trees.GetLength(0); i++) {
-                for(int j = 0; j < trees.GetLength(1); j++) {
-                    if(trees[i, j].Visible)
-                        visible++;
-                }
-            }
+			int visible = 0;
+			//Count the amount of visible trees
+			for(int i = 0; i < trees.GetLength(0); i++) {
+				for(int j = 0; j < trees.GetLength(1); j++) {
+					if(trees[i, j].Visible)
+						visible++;
+				}
+			}
 
-            Console.WriteLine(visible);
-        }
+			Console.WriteLine(visible);
+		}
+
+		/// <summary>
+		/// Find the tree with the highest scenic score, see the furthest away from itself.
+		/// </summary>
+		/// <param name="trees">All the <see cref="Tree"/> objects in two dimensional <see cref="Array"/>.</param>
+		static void Part2(Tree[,] trees) {
+			for(int i = 1; i < trees.GetLength(0) - 1; i++) {
+				for(int j = 1; j < trees.GetLength(1) - 1; j++) {
+					//Check left
+					for(int k = j - 1; k >= 0; k--) {
+						trees[i, j].Scenic[0]++;
+
+						//Break when a taller tree is found
+						if(trees[i, k].Height >= trees[i, j].Height)
+							break;
+					}
+					
+					//Check up
+					for(int k = i - 1; k >= 0; k--) {
+						trees[i, j].Scenic[1]++;
+
+						//Break when a taller tree is found
+						if(trees[k, j].Height >= trees[i, j].Height)
+							break;
+					}
+
+					//Check right
+					for(int k = j + 1; k <= trees.GetLength(1) - 1; k++) {
+						trees[i, j].Scenic[2]++;
+
+						//Break when a taller tree is found
+						if(trees[i, k].Height >= trees[i, j].Height)
+							break;
+					}
+
+					//Check down
+					for(int k = i + 1; k <= trees.GetLength(0) - 1; k++) {
+						trees[i, j].Scenic[3]++;
+
+						//Break when a taller tree is found
+						if(trees[k, j].Height >= trees[i, j].Height)
+							break;
+					}
+				}
+			}
+
+			int highest = 0;
+			//Find the tree with the highest scenic score
+			for(int i = 0; i < trees.GetLength(0); i++) {
+				for(int j = 0; j < trees.GetLength(1); j++) {
+					if(trees[i, j].ScenicScore() > highest)
+						highest = trees[i, j].ScenicScore();
+				}
+			}
+
+			Console.WriteLine(highest);
+		}
 
 		/// <summary>
 		/// A single tree with <see cref="height"/> and <see cref="visible"/> attributes.
@@ -87,6 +145,7 @@
 		class Tree {
 			private readonly int height;
 			private bool visible;
+			private int[] scenic;
 
 			/// <summary>
 			/// Set the height of the tree and if it is visible.
@@ -96,10 +155,33 @@
 			public Tree(char height, bool visible = false) {
 				this.height = height - '0';
 				this.visible = visible;
+				scenic = new int[4];
 			}
 
+			/// <summary>
+			/// The height of the tree as <see cref="int"/>.
+			/// </summary>
 			public int Height { get => height; }
+			/// <summary>
+			/// If the tree is visible or not from the outside as <see cref="bool"/>.
+			/// </summary>
 			public bool Visible { get => visible; set => visible = value; }
+			/// <summary>
+			/// The scenic score in each direction from the <see cref="Tree"/>.
+			/// </summary>
+			public int[] Scenic { get => scenic; set => scenic = value; }
+
+			/// <summary>
+			/// Mulitply the 4 different <see cref="scenic"/> scores together.
+			/// </summary>
+			/// <returns>The total scenic score as <see cref="int"/>.</returns>
+			public int ScenicScore() {
+				int scenicScore = 1;
+				foreach(int score in scenic)
+					scenicScore *= score;
+				
+				return scenicScore;
+			}
 		}
 	}
 }
