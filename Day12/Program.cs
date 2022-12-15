d77beb222001659ed82f3cf211e8f0f82434637a
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-12-15 23:55:19 +0100

    Add Day12 Dijkstra

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -6,22 +6,45 @@
 			string[] data = File.ReadAllLines(file);
 
 			List<Coordinate> map = new();
-			Coordinate start;
-			Coordinate end;
+			Coordinate start = null;
+			Coordinate end = null;
 
 			for(int x = 0; x < data.GetLength(0); x++) {
 				for(int y = 0; y < data[x].Length; y++) {
 					if(data[x][y] == 'S') {
-						start = new Coordinate(x, y, 'a');
+						start = new(x, y, 'a');
 						map.Add(start);
 					} else if(data[x][y] == 'E') {
-						end = new Coordinate(x, y, 'z');
+						end = new(x, y, 'z');
+						end.Distance = 0;
 						map.Add(end);
 					} else
 						map.Add(new Coordinate(x, y, data[x][y]));
 				}
 			}
-		}
+
+			PriorityQueue<Coordinate, int> queue = new();
+			queue.Enqueue(end!, (int)end!.Distance!);
+
+			while(queue.Count != 0) {
+				Coordinate coord = queue.Dequeue();
+
+				foreach(var neighbour in Neighbours(map, coord)) {
+					if(neighbour == null)
+						continue;
+
+					if(Math.Abs(neighbour.Height - coord.Height) < 2) {
+                        int distance = (int)(coord.Distance! + 1);
+                        if(distance < neighbour.Distance || neighbour.Distance == null) {
+                            neighbour.Distance = distance;
+                            queue.Enqueue(neighbour, (int)neighbour.Distance);
+                        }
+                    }
+				}
+			}
+
+            Console.WriteLine(start!.Distance);
+        }
 
 		/// <summary>
 		/// Represent a single coordinate on the map with its height and the distance from the end point.
@@ -36,7 +59,18 @@
 			public int X { get; }
 			public int Y { get; }
 			public int Height { get; }
-			public int Distance { get; set; }
+			public int? Distance { get; set; }
+		}
+
+		static IEnumerable<Coordinate> Neighbours(List<Coordinate> map, Coordinate coord) {
+			Coordinate?[] neighbours = new Coordinate[4];
+
+			neighbours[0] = map.SingleOrDefault(pos => pos.X == coord.X - 1 && pos.Y == coord.Y);
+			neighbours[1] = map.SingleOrDefault(pos => pos.X == coord.X + 1 && pos.Y == coord.Y);
+			neighbours[2] = map.SingleOrDefault(pos => pos.X == coord.X && pos.Y == coord.Y - 1);
+			neighbours[3] = map.SingleOrDefault(pos => pos.X == coord.X && pos.Y == coord.Y + 1);
+
+			return neighbours;
 		}
 	}
 }
